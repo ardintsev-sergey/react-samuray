e72d24d549f8e423d45b2b7763749f7ebc588973
Ardintsev Sergey <archunter13@yandex.ru>
2021-10-12 06:23:51 +0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+  <header className='header'>
+    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI5vFX9tv64JjrobsjPi_62hz5WBWU0FXjfA&usqp=CAU' alt="logo" />
+  </header>
+  )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTit-JnxMDv2GGAkG9_6lPM1695xErnnRg0Ng&usqp=CAU' />
+    </div>
+  )
+}
+
+export default Profile;
